6759e2a3eb77575e54d3c20d96a573a4884ad49f
Sunilworks <sunil.kumar.sh9217@gmail.com>
2023-09-03 13:23:28 +0530

    code change

--- src/Handson 2/FormClass.js
+++ src/Handson 2/FormClass.js
@@ -1,44 +1,105 @@
-import React from "react";
-import { Component } from "react";
+import React, { Component } from "react";
 
-class FormClass extends Component{
-    state = {
-        Name: ' ',
-        EmpData : []
-    }
-    changeHandle = (event) =>{
-        this.setState({[event.target.name] : event.target.value})
-        console.log(this.state.Name)
-    }   
-    clickHandle = () =>{
-        let newObj ={
-            name: this.state.Name
-        }
-        this.state.EmpData.push(newObj);
-        this.setState({EmpData: this.state.EmpData}) //To Update the Array we have to write this code usind updating State
-        console.log(this.state.EmpData);
-    }
-    render(){
-        return(
-            <>
-            <form>
-            <label htmlFor='name'>Name:</label>
-            <input id="name" type="text" placeholder="Enter Name" name="Name" value={this.state.Name} onChange={this.changeHandle} required/>
+export class Formclass extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Name: "",
+      Dept: "",
+      Rat: "",
+      EmpData: [],
+    };
+    // console.log(this.state);
+  }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value }); //To dynamicaly set the key value pair in javascript
+    // console.log(this.state);
+    // console.log(event);
+    // console.log(event.target.value);
+  };
 
-            <button type='button' onClick={this.clickHandle}>Submit</button>
-            </form>
-            {
-                this.state.EmpData.map((item,index) =>{
-                    return(
-                        <>
-                        <span>Name: {item.name} </span>
-                        </>
-                    )
-                })
-            }
-            </>
-        )
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const tempObj = {
+      name: this.state.Name,
+      department: this.state.Dept,
+      rating: this.state.Rat
+    };
+    this.state.EmpData.push(tempObj);
+    this.setState({
+      EmpData: this.state.EmpData,
+      Name: "",
+      Dept: "",
+      Rat: ""
+    });
+    // console.log(this.state.EmpData);
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="head">EMPLOYEE FEEDBACK FORM</h1>
+        <form className="form">
+          <label htmlFor="name">Name :</label>
+          <input
+            id="name"
+            placeholder="Enter Your Name"
+            type="text"
+            name="Name"
+            onChange={this.handleChange}
+            value={this.state.Name}
+            required
+          />
+
+          <br />
+
+          <label htmlFor="dept">Department :</label>
+          <input
+            id="dept"
+            placeholder="Enter Your Dept"
+            type="text"
+            name="Dept"
+            onChange={this.handleChange}
+            value={this.state.Dept}
+            required
+          />
+
+          <br />
+
+          <label htmlFor="rat">Rating :</label>
+          <input
+            id="rat"
+            placeholder="Enter Rating"
+            type="number"
+            required
+            name="Rat"
+            onChange={this.handleChange}
+            value={this.state.Rat}
+          />
+
+          <br />
+
+          <button type="submit" onClick={this.handleSubmit}>
+            Submit
+          </button>
+        </form>
+
+        <div className="dataBox">
+          {this.state.EmpData.map((item, index) => {
+            return (
+              <div key={index} className="displayBox">
+                <span>Name: {item.name} || </span>
+                <span>Department: {item.department} || </span>
+                <span>Rating: {item.rating}</span>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
 
-export default FormClass;
+// export default Formclass;
+
+//form validation package formic,yup
